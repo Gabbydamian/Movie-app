65e9cb65b624d2c6f0fe05e77e33cf3741ec3528
Gabriel Damian <gabbydamian92@gmail.com>
2022-07-23 17:38:21 +0100

    Uploading previous project

---
+++ app.js
@@ -0,0 +1,47 @@
+const API_KEY = 'api_key=02fbfc73d6702f6b0c3b8e7711a97e34';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
+const IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
+const main = document.getElementById('main')
+
+
+
+getMovies(API_URL);
+
+function getMovies(url) {
+    fetch(url).then(res => res.json()).then(data => {
+        console.log(data)
+        showMovies(data.results);
+    })
+}
+
+function showMovies(data) {
+    main.innerHTML = "";
+    data.forEach(movie => {
+        const{title, poster_path, vote_average, overview} = movie;
+        const movie1 = document.createElement('div');
+        movie1.classList.add('movie');
+        movie1.innerHTML = `
+        <img src="${IMAGE_URL + poster_path}" alt="${title}" />
+        <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getColor(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+            ${overview};
+        </div>
+        `
+
+        main.appendChild(movie1)
+    });
+}
+
+function getColor(vote) {
+    if (vote >= 8){
+        return `green`
+    }else if(vote >= 5){
+        return `orange`
+    }else{
+        return `red`
+    }
+}
